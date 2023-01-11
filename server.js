5c95c75bdd131a4a6d1f073756df783b8bc9fef7
Gus Lascola <augustolascola@gmail.com>
2023-01-11 14:35:02 -0500

    added file structure. Next step is to add what little work I've done

---
+++ server.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    startQuestions();
+});
+ 
